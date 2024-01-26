bdd6bcc97df92180f629f2380072d7b75c5da3cb
Eugenekulik <67892037+Eugenekulik@users.noreply.github.com>
2024-01-26 12:46:22 +0300

    Init project task1

---
+++ task1/src/main/java/by/eugenekulik/App.java
@@ -0,0 +1,13 @@
+package by.eugenekulik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
