992e358fda65bf29f7ae78099da6f1123515882b
Eugenekulik <67892037+Eugenekulik@users.noreply.github.com>
2024-02-11 23:46:33 +0300

    Changed docker-compose.yaml, created Dockerfile. They moved to docker directory. Update pom.xml. Refactor DataSource and MigrationService.

--- task1/src/main/java/by/eugenekulik/out/dao/jdbc/utils/DataSource.java
+++ task1/src/main/java/by/eugenekulik/out/dao/jdbc/utils/DataSource.java
@@ -7,17 +7,25 @@ import lombok.Getter;
 public class DataSource {
 
     private final String url;
+    private final String name;
+    private final String port;
     private final String user;
     private final String password;
 
-    public DataSource(String url, String user, String password, String driverName){
+    public DataSource(String url, String name, String port, String user, String password, String driverName) {
         this.url = url;
         this.user = user;
         this.password = password;
+        this.name = name;
+        this.port = port;
         try {
             Class.forName(driverName);
         } catch (ClassNotFoundException e) {
             throw new DatabaseInterectionException("Error to load driver class.", e);
         }
+    }
+
+    public String createUrl() {
+        return url + ":" + port + "/" + name;
     }
 }
--- task1/src/main/java/by/eugenekulik/service/MigrationService.java
+++ task1/src/main/java/by/eugenekulik/service/MigrationService.java
@@ -1,27 +1,34 @@
 package by.eugenekulik.service;
 
 import by.eugenekulik.out.dao.jdbc.utils.DataSource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import liquibase.Liquibase;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import lombok.NoArgsConstructor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@ApplicationScoped
+@NoArgsConstructor
 public class MigrationService {
-    private final DataSource dataSource;
+    private DataSource dataSource;
 
+    @Inject
     public MigrationService(DataSource dataSource) {
         this.dataSource = dataSource;
+        applyChanges();
     }
 
-    public void applyChanges(){
+    public void applyChanges() {
         try {
-            Connection connection = DriverManager.getConnection(dataSource.getUrl(),
+            Connection connection = DriverManager.getConnection(dataSource.createUrl(),
                 dataSource.getUser(),
                 dataSource.getPassword());
             Database database = DatabaseFactory.getInstance()
